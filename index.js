04527add3e0a2566597ba15a1c25b525d3e4121c
fadimeteir <fmeteir@gmail.com>
2019-09-18 16:19:24 +0300

    upload file

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log(23 + 97);
+console.log(1 + 2 + 3 + 4 + 5 + 6 + 7);
+console.log((4 + 6 + 9) / 77);
+
+const a = 10;
+console.log('A * 9 = ', 9 * a);
+
+const b = 7 * a;
+console.log('B * 7 = ', b);
+
+const max = 57;
+console.log('MAX = ', max);
+
